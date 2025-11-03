1c08c658d65474dd6c921fc3df6e7467b04385e7
24021479-ChuaXinfang <24021479@myrp.edu.sg>
2025-11-03 12:59:46 +0800

    Edited index.js and added index.css

--- src/index.js
+++ src/index.js
@@ -1,54 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"
 
 function App() {
     return (
-    <>
+    <div className="container">
     <Header/>
-    <Pizza/>
-    </>
+    <Menu/>
+    <Footer/>
+    </div>
     );
 }
 
 function Header() { // follow naming convention
-    return <h1>Xinfang's Pizza Co.</h1>;
+    return <h1 style={{color:"orange", fontSize: "48px", textTransform:"uppercase"}}>Xinfang's Pizza Co.</h1>;
 }
 
 function Pizza() {
     return (
-        <>
-        <img src="pizzas/spinaci.jpg"></img>
-        <h4>Spinaci Pizza</h4>
+        <div>
+        <img src="pizzas/spinaci.jpg" alt="Spinach Pizza"/>
+        <h3>Spinaci Pizza</h3>
         <p>Tomato, mozarella, spinach, and ricotta cheese</p>
         <p>10</p>
-        
-        <img src="pizzas/funghi.jpg"></img>
-        <h4>Funghi Pizza</h4>
-        <p>Tomato, mozarella, mushrooms, and onion</p>
-        <p>12</p>
-
-        <img src="pizzas/salamino.jpg"></img>
-        <h4>Salamino Pizza</h4>
-        <p>Tomato, mozarella, and pepperoni</p>
-        <p>15</p>
-
-        <img src="pizzas/prosciutto.jpg"></img>
-        <h4>Prosciutto Pizza</h4>
-        <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-        <p>18</p>
-
-        <img src="pizzas/focaccia.jpg"></img>
-        <h4>Focaccia Pizza</h4>
-        <p>Bread with italian olive oil and rosemary</p>
-        <p>6</p>
-
-        <img src="pizzas/margherita.jpg"></img>
-        <h4>Margherita Pizza</h4>
-        <p>Tomato and mozarella</p>
-        <p>10</p>
-        </>
+        </div>
     );
 }
+
+function Menu() {
+    return(
+        <div className="menu">
+        <h2>Our Menu</h2>
+        <Pizza/>
+        <Pizza/>
+        </div>
+    );
+}
+
+
+function Footer() {
+    const currentHour = new Date().getHours();
+    let message;
+
+    if (currentHour >= 10 && currentHour < 22) {
+        message = "We're currently open";
+    } else {
+        message = "Sorry, we're closed";
+    }
+    return(
+        <footer className="footer"><p>{message}</p></footer>
+    );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
